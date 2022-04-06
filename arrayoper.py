efd4fbd50bd7a02482626241d02fbed7402b9a0d
aduquet <duquet@ut.ee>
2022-04-06 09:08:25 +0300

    update

--- arrayoper.py
+++ arrayoper.py
@@ -8,35 +8,37 @@ class Array_oper(object):
         self.array_a = a 
         self.array_b = b
         
-    # @classmethod
-    # def array
-        
     def printArray(self):
         print(self.array_a, '\n')
         print(self.array_b, '\n')
         
     def add(self):
         
-        sum = [x + y for x, y in zip(self.array_a, self.array_b)]
-        
-        return sum
+        if self.equalSize():
+            sum = [x + y for x, y in zip(self.array_a, self.array_b)]
+            return sum
+        else:
+            return 'arrays must have the same size'
         
     def sub(self):
         
-        diff = [x - y for x, y in zip(self.array_a, self.array_b)]
-        
-        return diff
+        if self.equalSize():
+            diff = [x - y for x, y in zip(self.array_a, self.array_b)]
+            
+            return diff
+        else:
+            return 'arrays must have the same size'
         
     def dot_product(self):
-        
         dp = 0
-        
-        for i in range(len(self.array_a)):
-            dp += (self.array_a[i]*self.array_b[i])
-        
-        print(self.isEmpty(true))
-        return dp
-    
+        if self.equalSize():
+            for i in range(len(self.array_a)):
+                dp += (self.array_a[i] * self.array_b[i])
+            # print(self.isEmpty(true))
+            return dp
+        else:
+            return 'arrays must have the same size'
+            
     def isEmpty(self, is_a):
         
         if is_a:
--- tracegen.py
+++ tracegen.py
@@ -1,16 +1,10 @@
-import os
-import pathlib
-import glob as gl
-import numpy as np
-import pandas as pd
+
 from arrayoper import *
 
 a = []
 # b = ['a',' d', 'v']
 b = [1,2,3]
 
-# Array_oper(a)
-# Array_oper(b)
 myClass = Array_oper(a,b)
 myClass.printArray()
 print('add', myClass.add())
